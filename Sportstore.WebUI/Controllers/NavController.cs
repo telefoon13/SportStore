94af16de85026073803cfd77423f540d6a18e91f
telefoon13 <Mike0528>
2018-03-08 10:44:38 +0100

    Menu

---
+++ Sportstore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,31 @@
+﻿using Sportstore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sportstore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+
+
+        // GET: Nav
+        public PartialViewResult Menu(String category = null)
+        {
+            ViewBag.SelectedCategory = category;
+
+            IEnumerable<String> categories = repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
